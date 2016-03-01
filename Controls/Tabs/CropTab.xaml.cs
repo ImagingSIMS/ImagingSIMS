def9fa7e5daf6ee7e17873ccd32a8fe51c8a15af
Jay Tarolli <jay5026@hotmail.com>
2016-02-29 19:56:34 -0800

    Added the CropTab to the Controls.Tabs namespace.

--- Controls/Tabs/CropTab.xaml.cs
+++ Controls/Tabs/CropTab.xaml.cs
@@ -1,29 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-using ImagingSIMS.Common;
 using ImagingSIMS.Common.Dialogs;
 using ImagingSIMS.Common.Math;
-using ImagingSIMS.Controls.Tabs;
 using ImagingSIMS.Data;
 using ImagingSIMS.Data.Imaging;
 
-namespace ImagingSIMS.Controls
+namespace ImagingSIMS.Controls.Tabs
 {
     /// <summary>
     /// Interaction logic for CropTab.xaml
