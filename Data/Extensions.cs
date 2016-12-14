b976df059b7954f148cd77eea34b12b46dfc6906
Jay Tarolli <jay5026@hotmail.com>
2016-12-14 09:09:17 -0800

    Added extension method for upscaling 2D double arrays. Same procedure as in Data2D.

--- Data/Extensions.cs
+++ Data/Extensions.cs
@@ -198,4 +198,112 @@ namespace ImagingSIMS.Data
             return true;
         }
     }
+    
+    public static class MatrixExtensionMethods
+    {
+        /// <summary>
+        /// Upscales the 2D doule matrix to the specified dimensions. Can not be used to downsample a matrix.
+        /// </summary>
+        /// <param name="matrix">2D double matrix to upscale.</param>
+        /// <param name="targetWidth">Upscaled width.</param>
+        /// <param name="targetHeight">Upscaled height.</param>
+        /// <param name="useBilinear">True to use bilinear interpolation, false to use bicubic interpolation.</param>
+        /// <returns>2D double matrix with the target dimensions.</returns>
+        public static double[,] Upscale(this double[,] matrix, int targetWidth, int targetHeight, bool useBilinear = true)
+        {
+            int lowResSizeX = matrix.GetLength(0);
+            int lowResSizeY = matrix.GetLength(1);
+            int highResSizeX = targetWidth;
+            int highResSizeY = targetHeight;
+
+            if (lowResSizeX > highResSizeX || lowResSizeY > highResSizeY)
+                throw new ArgumentException("Cannot downscale data using this function.");
+
+            if (useBilinear)
+                return UpscaleBilinear(matrix, lowResSizeX, lowResSizeY, highResSizeX, highResSizeY);
+            else
+                return UpscaleBicubic(matrix, lowResSizeX, lowResSizeY, highResSizeX, highResSizeY);
+        }
+        private static double[,] UpscaleBilinear(double[,] matrix, int lowResSizeX, int lowResSizeY, int highResSizeX, int highResSizeY)
+        {
+            double[,] resized = new double[highResSizeX, highResSizeY];
+
+            double A, B, C, D;
+            int x, y;
+            double xRatio = (lowResSizeX - 1d) / highResSizeX;
+            double yRatio = (lowResSizeY - 1d) / highResSizeY;
+            double xDiff, yDiff;
+
+            for (int i = 0; i < highResSizeX; i++)
+            {
+                for (int j = 0; j < highResSizeY; j++)
+                {
+                    x = (int)(xRatio * i);
+                    y = (int)(yRatio * j);
+                    xDiff = (xRatio * i) - x;
+                    yDiff = (yRatio * j) - y;
+
+                    int x1 = x + 1;
+                    if (x1 >= highResSizeX) x1 = x;
+                    int y1 = y + 1;
+                    if (y1 >= highResSizeY) y1 = y;
+
+                    A = matrix[x, y];
+                    B = matrix[x + 1, y];
+                    C = matrix[x, y + 1];
+                    D = matrix[x + 1, y + 1];
+
+                    resized[i, j] = (A * (1d - xDiff) * (1d - yDiff)) + (B * (xDiff) * (1d - yDiff)) +
+                        (C * (yDiff) * (1d - xDiff)) + (D * (xDiff) * (yDiff));
+                }
+            }
+
+            return resized;
+        }
+        private static double[,] UpscaleBicubic(double[,] matrix, int lowResSizeX, int lowResSizeY, int highResSizeX, int highResSizeY)
+        {
+            double[,] resized = new double[highResSizeX, highResSizeY];
+
+            double xRatio = (lowResSizeX - 1d) / highResSizeX;
+            double yRatio = (lowResSizeY - 1d) / highResSizeY;
+
+            for (int i = 0; i < highResSizeX; i++)
+            {
+                for (int j = 0; j < highResSizeY; j++)
+                {
+                    int x = (int)(xRatio * i);
+                    int y = (int)(yRatio * j);
+
+                    int[] xCoords = new int[]
+                    {
+                        x, Math.Min(x + 1, x), Math.Min(x + 2, x), Math.Min(x + 3, x)
+                    };
+                    int[] yCoords = new int[]
+                    {
+                        y, Math.Min(y + 1, y), Math.Min(y + 2, y), Math.Min(y + 3, y)
+                    };
+
+                    double[] xv = new double[4];
+                    for (int k = 0; k < 4; k++)
+                    {
+                        xv[k] = InterpolateCubic(matrix[xCoords[0], yCoords[k]],
+                            matrix[xCoords[1], yCoords[k]], matrix[xCoords[2], yCoords[k]], matrix[xCoords[3], yCoords[k]], xRatio);
+                    }
+
+                    resized[i, j] = InterpolateCubic(xv[0], xv[1], xv[2], xv[3], yRatio);
+                }
+            }
+
+            return resized;
+        }
+        private static double InterpolateCubic(double v0, double v1, double v2, double v3, double frac)
+        {
+            double a = (v3 - v2) - (v0 - v1);
+            double b = (v0 - v1) - a;
+            double c = v2 - v0;
+            double d = v1;
+
+            return d + frac * (c + frac * (b + frac * a));
+        }
+    }
 }
