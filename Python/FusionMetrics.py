cac459f8fe115fb6ffc805701cf2d4fe8a2f4ccb
Jay Tarolli <jay5026@hotmail.com>
2017-09-07 12:52:52 -0700

    Continue fusion metrics

--- Python/FusionMetrics.py
+++ Python/FusionMetrics.py
@@ -8,10 +8,11 @@ def standardDeviation(fusedImage):
     gray = (fusedImage[:,:,0] + fusedImage[:,:,1] + fusedImage[:,:,2]) / 3
     gray = np.divide(gray, np.max(gray))
     hist, bins = np.histogram(np.reshape(gray, [-1]), bins=100, density=True)
+    hist = np.divide(hist, np.sum(hist))
     bins = bins[:-1]
 
     iBar = np.sum(bins * hist)
-    stDev = np.sqrt(np.sum((bins - iBar) * hist))
+    stDev = np.sqrt(np.sum(np.power((bins - iBar), 2) * hist))
 
     return stDev
 
@@ -20,6 +21,7 @@ def informationEntropy(fusedImage):
     gray = (fusedImage[:,:,0] + fusedImage[:,:,1] + fusedImage[:,:,2]) / 3
     gray = np.divide(gray, np.max(gray))
     hist, bins = np.histogram(np.reshape(gray, [-1]), bins=100, density=True)
+    hist = np.divide(hist, np.sum(hist))
     bins = bins[:-1]
 
     # Add small amout to avoid nan
@@ -35,14 +37,17 @@ def crossEntropy(highresImage, lowresImage, fusedImage):
     grayF = (fusedImage[:,:,0] + fusedImage[:,:,1] + fusedImage[:,:,2]) / 3
     grayF = np.divide(grayF, np.max(grayF))
     histF, binsF = np.histogram(np.reshape(grayF, [-1]), bins=100, density=True)
+    histF = np.divide(histF, np.sum(histF))
 
     grayL = (lowresImage[:,:,0] + lowresImage[:,:,1] + lowresImage[:,:,2]) / 3
     grayL = np.divide(grayL, np.max(grayL))
     histL, binsL = np.histogram(np.reshape(grayL, [-1]), bins=100, density=True)
+    histL = np.divide(histL, np.sum(histL))
 
     grayH = (highresImage[:,:,0] + highresImage[:,:,1] + highresImage[:,:,2]) / 3
     grayH = np.divide(grayH, np.max(grayH))
     histH, binsH = np.histogram(np.reshape(grayH, [-1]), bins=100, density=True)
+    histH = np.divide(histH, np.sum(histH))
 
     bins = binsF[:-1]
 
@@ -65,12 +70,17 @@ def mutualInformation(highresImage, lowresImage, fusedImage):
     grayH = np.reshape((highresImage[:,:,0] + highresImage[:,:,1] + highresImage[:,:,2]) / 3, [-1])
     grayL = np.divide(grayH, np.max(grayH))
 
-    jdfFL, binsFLx, binsFLy = np.histogram2d(grayF, grayL, bins=100, normed=True)
-    jdfFH, binsFHx, binsFHy = np.histogram2d(grayF, grayH, bins=100, normed=True)
+    jdfFL, binsFLx, binsFLy = np.histogram2d(grayF, grayL, bins=(100, 100), normed=False)
+    jdfFL = np.divide(jdfFL, np.sum(jdfFL))
+    jdfFH, binsFHx, binsFHy = np.histogram2d(grayF, grayH, bins=(100, 100), normed=False)
+    jdfFH = np.divide(jdfFH, np.sum(jdfFH))
 
     pdfF, binsF = np.histogram(grayF, bins=100, density=True)
+    pdfF = np.divide(pdfF, np.sum(pdfF))
     pdfL, binsL = np.histogram(grayL, bins=100, density=True)
+    pdfL = np.divide(pdfL, np.sum(pdfL))
     pdfH, binsH = np.histogram(grayH, bins=100, density=True)
+    pdfH = np.divide(pdfH, np.sum(pdfH))
 
     bins = binsF[:-1]
 
@@ -81,10 +91,22 @@ def mutualInformation(highresImage, lowresImage, fusedImage):
     pdfL = pdfL + 1e-10
     pdfH = pdfH + 1e-10
 
-    iFL = np.sum(jdfFL * np.log2(jdfFL / np.dot(pdfF, pdfL)))
-    iFH = np.sum(jdfFH * np.log2(jdfFH / np.dot(pdfF, pdfH)))
+    #iFL = np.sum(jdfFL * np.log2(jdfFL / np.dot(pdfF, pdfL)))
+    #iFH = np.sum(jdfFH * np.log2(jdfFH / np.dot(pdfF, pdfH)))
 
-    return iFL + iFH
+    sumFH = 0
+    sumFL = 0
+
+    for f in range(len(bins)):
+        for h in range(len(bins)):
+            sumFH = sumFH + jdfFH[f, h] * np.log2(jdfFH[f, h] / (pdfF[f] * pdfH[h]))
+
+    for f in range(len(bins)):
+        for l in range(len(bins)):
+            sumFL = sumFL + jdfFL[f, l] * np.log2(jdfFL[f, l] / (pdfF[f] * pdfL[l]))            
+
+    # return iFL + iFH
+    return sumFH + sumFL
 
 def crossCorrelation(lowresImage, fusedImage):
 
@@ -103,6 +125,8 @@ def crossCorrelation(lowresImage, fusedImage):
     rMeanF = np.mean(rF)
     gMeanF = np.mean(gF)
     bMeanF = np.mean(bF)
+
+    cc = np.zeros([3])
 
     cc[0] = np.sum((rF - rMeanF) * (rL - rMeanL)) / np.sqrt(np.sum(np.square(rF - rMeanF)) * np.sum(np.square(rL - rMeanL)))
     cc[1] = np.sum((gF - gMeanF) * (gL - gMeanL)) / np.sqrt(np.sum(np.square(gF - gMeanF)) * np.sum(np.square(gL - gMeanL)))
