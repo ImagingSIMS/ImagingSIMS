ce8f9db29cd44b1989b0cf994faf21a1161e2e12
Jay Tarolli <jay.tarolli@gmail.com>
2023-12-18 17:43:48 -0800

    Add tab for dynamic overlays

---
+++ Controls/Tabs/ImageOverlay.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Accord.IO;
+using ImagingSIMS.Common.Dialogs;
+using ImagingSIMS.Controls.ViewModels;
+using ImagingSIMS.Data;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImagingSIMS.Controls.Tabs
+{
+    /// <summary>q
+    /// Interaction logic for ImageOverlay.xaml
+    /// </summary>
+    public partial class ImageOverlay : UserControl, INotifyPropertyChanged
+    {
+        ImageOverlayViewModel _viewModel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ImageOverlayViewModel ViewModel
+        {
+            get { return _viewModel; }
+            set
+            {
+                if (_viewModel != value)
+                {
+                    _viewModel = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+        }
+
+
+        public ImageOverlay(Workspace workspace)
+        {
+            ViewModel = new ImageOverlayViewModel();
+            ViewModel.Workspace = workspace;
+            DataContext = ViewModel;
+
+            InitializeComponent();
+        }
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private void buttonAddImage_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.AddImage();
+        }
+        private void buttonRemoveImage_Click(object sender, RoutedEventArgs e)
+        {
+            var settings = ((Button)sender).DataContext as OverlaySettingsViewModel;
+            ViewModel.RemoveImage(settings);
+        }
+        private void comboBoxImage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var widths = ViewModel.Images.Select(i => i.Image?.Width)
+                .Where(w => w.HasValue).Distinct();
+            var heights = ViewModel.Images.Select(i => i.Image?.Height)
+                .Where(h => h.HasValue).Distinct();
+
+            if (widths.Count() > 1 || heights.Count() > 1)
+            {
+                DialogBox.Show(
+                    "Invalid image shape",
+                    "Not all selected images have the same image dimensions. Only images of the same dimensions can be overlaid.",
+                    "Error",
+                    DialogIcon.Error);
+                return;
+            }
+        }
+
+        private void contentButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Bitmap Images (.bmp)|*.bmp|PNG Images (.png)|*.png|Jpeg Images (.jpeg,.jpeg)|*.jpg;*.jpeg|Tiff Images (.tiff,.tif)|*.tif;*.tiff";
+
+            if (sfd.ShowDialog() != true) return;
+
+            if (ViewModel.OverlayImage == null) return;
+
+            ViewModel.OverlayImage.Save(sfd.FileName);
+        }
+        private void contentButtonCopy_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bs = ViewModel.OverlayImage as BitmapSource;
+            if (bs == null) return;
+
+            Clipboard.SetImage(bs);
+        }
+    }
+}
---
+++ Controls/ViewModels/ImageOverlayViewModel.cs
@@ -0,0 +1,193 @@
+﻿using ImagingSIMS.Common.Dialogs;
+using ImagingSIMS.Data;
+using ImagingSIMS.Data.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ImagingSIMS.Controls.ViewModels
+{
+    public class OverlaySettingsViewModel : INotifyPropertyChanged
+    {
+        Data2D _image;
+        Color _color;
+        double _saturation;
+        double _threshold;
+
+        public Data2D Image
+        {
+            get { return _image; }
+            set
+            {
+                if (_image != value)
+                {
+                    _image = value;
+                    NotifyPropertyChanged();
+                    Saturation = Image.Maximum;
+                    Threshold = 0;
+                }
+            }
+        }
+        public Color Color
+        {
+            get { return _color; }
+            set
+            {
+                if ( _color != value)
+                {
+                    _color = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double Saturation
+        {
+            get { return _saturation; }
+            set
+            {
+                if (_saturation != value)
+                {
+                    _saturation = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double Threshold
+        {
+            get { return _threshold; }
+            set
+            {
+                if ( _threshold != value)
+                {
+                    _threshold = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand RemoveCommand { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler SettingsChanged;
+
+        public OverlaySettingsViewModel()
+        {
+            Color = Color.FromRgb(255, 255, 255);
+        }
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            SettingsChanged?.Invoke(this, EventArgs.Empty);            
+        }
+    }
+    public class ImageOverlayViewModel : INotifyPropertyChanged
+    {
+        Workspace _workspace;
+        ObservableCollection<OverlaySettingsViewModel> _images;
+        ImageSource _overlayImage;
+
+        public Workspace Workspace
+        {
+            get { return _workspace; }
+            set
+            {
+                if ( _workspace != value)
+                {
+                    _workspace = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public ObservableCollection<OverlaySettingsViewModel> Images
+        {
+            get { return _images; }
+            set
+            {
+                if (_images != value)
+                {
+                    _images = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public ImageSource OverlayImage
+        {
+            get { return _overlayImage; }
+            set
+            {
+                if (_overlayImage != value)
+                {
+                    _overlayImage = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ImageOverlayViewModel()
+        {
+            Images = new ObservableCollection<OverlaySettingsViewModel>();
+        }
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void AddImage()
+        {
+            var setting = new OverlaySettingsViewModel() { Color = Color.FromRgb(255, 255, 255) };
+            setting.SettingsChanged += Setting_SettingsChanged;
+            Images.Add(setting);
+        }
+
+        private void Setting_SettingsChanged(object sender, EventArgs e)
+        {
+            UpdateOverlay();
+        }
+
+        public void RemoveImage(OverlaySettingsViewModel setting)
+        {
+            setting.SettingsChanged -= Setting_SettingsChanged;
+            Images.Remove(setting);
+
+            UpdateOverlay();
+        }
+
+        private void UpdateOverlay()
+        {
+            var toOverlay = Images.Where(s => s.Image != null).ToList();
+
+            var widths = toOverlay.Select(i => i.Image?.Width)
+                .Where(w => w.HasValue).Distinct();
+            var heights = toOverlay.Select(i => i.Image?.Height)
+                .Where(h => h.HasValue).Distinct();
+
+            if (widths.Count() > 1 || heights.Count() > 1) return;
+
+            var images = Images.Where(i => i.Image != null).Select(i => i.Image).ToArray();
+            var colors = Images.Where(i => i.Image != null).Select(i => i.Color).ToArray();
+            var saturations = Images.Where(i => i.Image != null).Select(i => (float)(i.Saturation)).ToArray();
+            var thresholds = Images.Where(i => i.Image != null).Select(i => (float)(i.Threshold)).ToArray();
+
+            if (images.Count() == 0)
+            {
+                OverlayImage = null;
+                return;
+            }
+
+            var overlay = Overlay.CreateOverlay(images, colors, saturations, thresholds);
+            overlay.Freeze();
+            OverlayImage = overlay;
+        }
+    }
+}
